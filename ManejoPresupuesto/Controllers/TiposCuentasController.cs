fd0c762613e1a60192a855460fdb9cb1b97ed398
Omar Garcia <omar.garcia@trinnova.mx>
2024-02-07 17:03:08 -0600

    Creando Controller, Model y View

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManejoPresupuesto.Models;
+
+namespace ManejoPresupuesto.Controllers
+{
+    /*
+        Se usa Control + . (punto)
+        para poder importar el NAMESPACE
+    */
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}

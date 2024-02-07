1a5e38a69496956691fd0c434180644df03ce795
Omar Garcia <omar.garcia@trinnova.mx>
2024-02-07 17:52:11 -0600

    Visualizando Errores de Validacion

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -5,7 +5,11 @@ namespace ManejoPresupuesto.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required]
+        /* Required(ErrorMessage = "El Nombre es obligatorio.")] */
+
+        /* {0}: hace referencia al campo de Nombre, por lo que en esa posición pone el nombre del campo */
+        [Required(ErrorMessage = "El {0} es obligatorio.")]
+        [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage ="Carácteres permitidos de: {2} a: {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }

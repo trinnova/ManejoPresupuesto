875b2bd2127241c9871f9a0b5f3a7e778e1fd0f3
Omar Garcia <omar.garcia@trinnova.mx>
2024-02-08 12:28:18 -0600

    Validaciones personalizadas por Modelo

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta //: IValidatableObject
     {
         public int Id { get; set; }
         // Required(ErrorMessage = "El Nombre es obligatorio.")]
@@ -18,6 +18,20 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula",
+        //                new[] { nameof(Nombre) });
+        //        }
+        //    }
+        //}
 
         // Otras pruebas de validación
         //[Required(ErrorMessage = "El campo: {0} es requerido")]
